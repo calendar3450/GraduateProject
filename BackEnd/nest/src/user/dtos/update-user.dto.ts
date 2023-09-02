f723b56612eb1c6927eea3a5b34a9aa4a6a1dea1
민수 <109027302+Pushedsu@users.noreply.github.com>
2023-09-02 11:52:50 +0900

    dto 삭제

--- BackEnd/nest/src/user/dtos/update-user.dto.ts
+++
@@ -1,7 +0,0 @@
-import { PickType } from '@nestjs/swagger';
-import { User } from '../entities/user.entity';
-
-export class UpdateUserDto extends PickType(User, [
-  'userId',
-  'userName',
-] as const) {}
